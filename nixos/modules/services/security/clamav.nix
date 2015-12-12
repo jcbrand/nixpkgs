cb83a030e6d343c422c50c9ce336dceedc4d6c81
JC Brand <jc@opkode.com>
2015-12-12 16:28:21 +0000

    Replace the old upstart "job" section with systemd.services
    Also remove unnecessary config options.

--- nixos/modules/services/security/clamav.nix
+++ nixos/modules/services/security/clamav.nix
@@ -8,88 +8,11 @@ let
   clamavGroup = clamavUser;
   cfg = config.services.clamav;
   clamdConfigFile = pkgs.writeText "clamd.conf" ''
-    LocalSocket ${runDir}/clamd.ctl
-    FixStaleSocket true
-    LocalSocketGroup clamav
-    LocalSocketMode 666
-    # TemporaryDirectory is not set to its default /tmp here to make overriding
-    # the default with environment variables TMPDIR/TMP/TEMP possible
-    User clamav
-    AllowSupplementaryGroups true
-    ScanMail true
-    ScanArchive true
-    ArchiveBlockEncrypted false
-    MaxDirectoryRecursion 15
-    FollowDirectorySymlinks false
-    FollowFileSymlinks false
-    ReadTimeout 180
-    MaxThreads 12
-    MaxConnectionQueueLength 15
-    LogSyslog false
-    LogRotate true
-    LogFacility LOG_LOCAL6
-    LogClean false
-    LogVerbose false
-    PidFile ${runDir}/clamd.pid
     DatabaseDirectory ${stateDir}
-    SelfCheck 3600
-    Foreground false
-    Debug false
-    ScanPE true
-    MaxEmbeddedPE 10M
-    ScanOLE2 true
-    ScanPDF true
-    ScanHTML true
-    MaxHTMLNormalize 10M
-    MaxHTMLNoTags 2M
-    MaxScriptNormalize 5M
-    MaxZipTypeRcg 1M
-    ScanSWF true
-    DetectBrokenExecutables false
-    ExitOnOOM false
-    LeaveTemporaryFiles false
-    AlgorithmicDetection true
-    ScanELF true
-    IdleTimeout 30
-    PhishingSignatures true
-    PhishingScanURLs true
-    PhishingAlwaysBlockSSLMismatch false
-    PhishingAlwaysBlockCloak false
-    PartitionIntersection false
-    DetectPUA false
-    ScanPartialMessages false
-    HeuristicScanPrecedence false
-    StructuredDataDetection false
-    CommandReadTimeout 5
-    SendBufTimeout 200
-    MaxQueue 100
-    ExtendedDetectionInfo true
-    OLE2BlockMacros false
-    ScanOnAccess false
-    AllowAllMatchScan true
-    ForceToDisk false
-    DisableCertCheck false
-    DisableCache false
-    MaxScanSize 100M
-    MaxFileSize 25M
-    MaxRecursion 10
-    MaxFiles 10000
-    MaxPartitions 50
-    MaxIconsPE 100
-    StatsEnabled false
-    StatsPEDisabled true
-    StatsHostID auto
-    StatsTimeout 10
-    StreamMaxLength 25M
+    LocalSocket ${runDir}/clamd.ctl
     LogFile ${logDir}/clamav.log
-    LogTime true
-    LogFileUnlock false
-    LogFileMaxSize 0
-    Bytecode true
-    BytecodeSecurity TrustSigned
-    BytecodeTimeout 60000
-    OfficialDatabaseOnly false
-    CrossFilesystems true
+    PidFile ${runDir}/clamd.pid
+    User clamav
 
     ${cfg.daemon.extraConfig}
   '';
@@ -162,30 +85,42 @@ in
       DatabaseMirror database.clamav.net
     '';
 
-    jobs = {
-      clamav_daemon = mkIf cfg.daemon.enable {
-	      name = "clamav-daemon";
-        startOn = "startup";
+    systemd.services.clamd = mkIf cfg.daemon.enable {
+      description = "ClamAV daemon (clamd)";
+      path = [ pkgs.clamav ];
+      after = [ "network.target" ];
+      wantedBy = [ "multi-user.target" ];
+      preStart = ''
+        mkdir -m 0755 -p ${logDir}
+        mkdir -m 0755 -p ${runDir}
+        chown ${clamavUser}:${clamavGroup} ${logDir}
+        chown ${clamavUser}:${clamavGroup} ${runDir}
+        '';
+      serviceConfig = {
+        ExecStart = "${pkgs.clamav}/bin/clamd -c ${clamdConfigFile}";
+        ExecReload = "${pkgs.coreutils}/bin/kill -HUP $MAINPID";
+        Restart = "on-failure";
+        RestartSec = "10s";
+        StartLimitInterval = "1min";
+      };
+    };
 
-        preStart = ''
-          mkdir -m 0755 -p ${logDir}
-          mkdir -m 0755 -p ${runDir}
-          chown ${clamavUser}:${clamavGroup} ${logDir}
-          chown ${clamavUser}:${clamavGroup} ${runDir}
-          '';
-        exec = "${pkgs.clamav}/bin/clamd -c ${clamdConfigFile}";
-      }; 
-      clamav_updater = mkIf cfg.updater.enable {
-	      name = "clamav-updater";
-        startOn = "started network-interfaces";
-        stopOn = "stopping network-interfaces";
-
-        preStart = ''
-          mkdir -m 0755 -p ${stateDir}
-          chown ${clamavUser}:${clamavGroup} ${stateDir}
-          '';
-        exec = "${pkgs.clamav}/bin/freshclam --daemon --config-file=${pkgs.writeText "freshclam.conf" cfg.updater.config}";
-      }; 
+    systemd.services.freshclam = mkIf cfg.updater.enable {
+      description = "ClamAV updater (freshclam)";
+      after = [ "network.target" ];
+      wantedBy = [ "multi-user.target" ];
+      path = [ pkgs.clamav ];
+      preStart = ''
+        mkdir -m 0755 -p ${stateDir}
+        chown ${clamavUser}:${clamavGroup} ${stateDir}
+        '';
+      serviceConfig = {
+        ExecStart = "${pkgs.clamav}/bin/freshclam --daemon --config-file=${pkgs.writeText "freshclam.conf" cfg.updater.config}";
+        ExecReload = "${pkgs.coreutils}/bin/kill -HUP $MAINPID";
+        Restart = "on-failure";
+        RestartSec = "10s";
+        StartLimitInterval = "1min";
+      };
     };
   };
 }
